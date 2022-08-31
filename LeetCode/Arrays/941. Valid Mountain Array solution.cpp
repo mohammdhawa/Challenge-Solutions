bd3568356edcb4ef7821bd4606bb8f9c6e0d11d2
Mohammad Hawa <66217824+mohammdhawa@users.noreply.github.com>
2022-08-31 21:31:02 +0300

    Create 941. Valid Mountain Array solution.cpp

---
+++ LeetCode/Arrays/941. Valid Mountain Array solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) {
+        int flag = 0, i = 0;
+    for (i = 1; i < arr.size(); i++)
+        if (arr[i] <= arr[i - 1])
+            break;
+ 
+    if (i == arr.size() || i == 1)
+        return false;
+ 
+    for (; i < arr.size(); i++)
+        if (arr[i] >= arr[i - 1])
+            break;
+    return i == arr.size();
+    }
+};
